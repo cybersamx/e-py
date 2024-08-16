a1b30a18d0eca2dac8b1c631fbc90e5c0009d727
cybersamx <cybersamx@gmail.com>
2024-08-16 16:41:58 -0700

    Simplify compare function and readme, v1.0.1

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -20,53 +20,40 @@ class AriTestCase:
     divExpected: float
 
 
-tcs = [
-    AriTestCase(0, 0, 0, 0, 0, math.nan),
-    AriTestCase(1, 0, 1, 1, 0, math.inf),
-    AriTestCase(0, 1, 1, -1, 0, 0),
-    AriTestCase(2, 5, 7, -3, 10, 0.4),
-    AriTestCase(5, 2, 7, 3, 10, 2.5),
-]
-
-
 def compare(a: float, b: float) -> bool:
-    a_nan = math.isnan(a)
-    b_nan = math.isnan(b)
-
-    if a_nan and b_nan:
-        return True
-    elif a_nan or b_nan:
-        return False
-
-    if math.isinf(a) and math.isinf(b):
-        return True
-
     epsilon = 0.00001
-    res = math.fabs(a - b)
-    return res < epsilon
+    return math.fabs(a-b) < epsilon
 
 
 class TestArithmetic(unittest.TestCase):
+    tcs = [
+        AriTestCase(0, 0, 0, 0, 0, math.nan),
+        AriTestCase(1, 0, 1, 1, 0, math.inf),
+        AriTestCase(0, 1, 1, -1, 0, 0),
+        AriTestCase(2, 5, 7, -3, 10, 0.4),
+        AriTestCase(5, 2, 7, 3, 10, 2.5),
+    ]
+
     def test_add(self):
-        for tc in tcs:
+        for tc in self.tcs:
             res = add(tc.a, tc.b)
             expect = tc.addExpected
             self.assertTrue(compare(res, expect))
 
     def test_subtract(self):
-        for tc in tcs:
+        for tc in self.tcs:
             res = subtract(tc.a, tc.b)
             expect = tc.subExpected
             self.assertTrue(compare(res, expect))
 
     def test_multiply(self):
-        for tc in tcs:
+        for tc in self.tcs:
             res = multiply(tc.a, tc.b)
             expect = tc.mulExpected
             self.assertTrue(compare(res, expect))
 
     def test_divide(self):
-        for tc in tcs:
+        for tc in self.tcs:
             try:
                 res = divide(tc.a, tc.b)
                 expect = tc.divExpected
